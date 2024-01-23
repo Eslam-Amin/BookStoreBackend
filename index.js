5decb0dfe0de81604447b7272108721638551b68
Eslam Amin <Eslam.amin.610@gmail.com>
2024-01-23 21:56:29 +0200

    Configuring CORS Option

--- index.js
+++ index.js
@@ -13,19 +13,21 @@ app.use(express.json());
 
 
 
-app.use("/books", booksRouter);
 
 
 //Middleware for handling CORS Policy
 //Option 1: Allow All Origins with default of cors(*)
-app.use(cors());
+//app.use(cors());
 //Option 2: Allow Custom Origins
-app.use(cors({
-    origin: "https://enchanting-axolotl-565769.netlify.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
     allowedHeaders: ["Content-Type"],
-}))
+}
+app.use(cors(corsOptions));
 
+app.use("/books", booksRouter);
 ///app.use(cors({ origin: "http://localhost:5173/" }))
 
 app.get("/", (req, res) => {
